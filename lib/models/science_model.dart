c8cc323bc9c1135e64b77a023e77db5149c785fd
Oussama Berredjem <oussamaprotect@gmail.com>
2024-06-25 15:20:44 +0100

    lomn

--- lib/models/science_model.dart
+++ lib/models/science_model.dart
@@ -1,13 +1,14 @@
-import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project2/files/scane.dart';
 import 'package:project2/models/science_object.dart';
+import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
 
 class ScienceModel extends StatelessWidget {
   ScienceObject science;
   VoidCallback ontap;
   ScienceModel({Key? key,required this.science,required this.ontap}) : super(key: key);
+  final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
 
 
 
@@ -34,8 +35,21 @@ class ScienceModel extends StatelessWidget {
               width: 55,
               child: FloatingActionButton(
                 backgroundColor: Colors.black26,
-                onPressed: () {
-                 Navigator.push(context, CupertinoPageRoute(builder: (context)=>Scane(idScience: science.id,qr: science.qr,)));
+                onPressed: () async{
+                  try{
+                    _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                        context: context,
+                        onCode: (code) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("success $code")));
+
+                        });
+
+                  }catch(e){
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("error $e")));
+
+                  }
+
+                  //Navigator.push(context, CupertinoPageRoute(builder: (context)=>Scane(idScience: science.id,qr: science.qr,)));
                 },
                 child: Icon(Icons.arrow_forward,color: Colors.white,),
               ),
